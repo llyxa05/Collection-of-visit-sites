3011f8a8daffe180336673fe955192cfad644027
ilyxa05 <77862722+llyxa05@users.noreply.github.com>
2022-05-16 14:26:18 +0200

    Add files via upload

---
+++ nk.ax/assets/main.js
@@ -0,0 +1,162 @@
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 40,
+        "density": {
+          "enable": true,
+          "value_area": 1500
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 10,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 450,
+        "color": "#ffffff",
+        "opacity": 2,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "bounce",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 200,
+          "line_linked": {
+            "opacity": 5
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+
+// this code is a mess :)))
+// kinda
+
+textToWrite = "";
+writingText = false;
+
+textCont = document.getElementById('clickContents');
+
+function WriteText(text) {
+    textToWrite = text;
+    writingText = true;
+}
+
+setInterval(() => {
+    if (writingText) {
+        if (textCont.innerText != textToWrite){
+            if (!textToWrite.startsWith(textCont.innerText)){
+                textCont.innerText = textCont.innerText.substring(0, textCont.innerText.length-1);
+            } else {
+                textCont.innerText += textToWrite[textCont.innerText.length];
+            }
+            if (textToWrite.startsWith("https")) {
+                textCont.href = textToWrite;
+            } else {
+                textCont.removeAttribute("href");
+            }
+        }
+    }
+}, 30);
+
+var cursor = true;
+var speed = 500;
+setInterval(() => {
+    if(cursor) {
+        document.getElementById('cursor').style.opacity = 0;
+        cursor = false;
+    } else {
+        document.getElementById('cursor').style.opacity = 1;
+        cursor = true;
+    }
+}, speed);
